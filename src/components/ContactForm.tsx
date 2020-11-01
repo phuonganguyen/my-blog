bd34ddc33c19446ba51f4f466da740c4d2a17984
alvin.nguyen <alvin.nguyen@luminetsoft.com>
2020-11-01 22:47:38 +0700

    add netlify form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -56,20 +56,30 @@ const Button = styled.input`
 const ContactForm: FunctionComponent = () => {
     return (
         <>
-            <form name="contact" action="/success" method="POST" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact" />
+            <form name="contact" method="POST" data-netlify="true">
                 <p>
-                    <input type="text" name="firstname" id="firstname" />
-                    <label htmlFor="yourname">Your Name:</label> <br />
-                    <input type="text" name="name" id="yourname" />
+                    <label>
+                        Your Name: <input type="text" name="name" />
+                    </label>
                 </p>
                 <p>
-                    <label htmlFor="youremail">Your Email:</label> <br />
-                    <input type="email" name="email" id="youremail" />
+                    <label>
+                        Your Email: <input type="email" name="email" />
+                    </label>
                 </p>
                 <p>
-                    <label htmlFor="yourmessage">Message:</label> <br />
-                    <textarea name="message" id="yourmessage"></textarea>
+                    <label>
+                        Your Role:{' '}
+                        <select name="role[]" multiple>
+                            <option value="leader">Leader</option>
+                            <option value="follower">Follower</option>
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message"></textarea>
+                    </label>
                 </p>
                 <p>
                     <button type="submit">Send</button>
